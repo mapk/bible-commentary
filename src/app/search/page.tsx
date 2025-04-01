a0f04277b52ef41f0e6ce3bacf4c8ccf02c35250
Mark Uraine <muraine@shopmonkey.io>
2025-03-31 23:13:43 -0700

    minor font size adjustment

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -82,7 +82,7 @@ export default function SearchPage() {
                 <div className="w-1/4">
                   <Button
                     variant="link"
-                    className="p-0 h-auto text-blue-600"
+                    className="p-0 h-auto text-base text-blue-600"
                     asChild
                   >
                     <Link
